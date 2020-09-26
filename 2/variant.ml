c04cadfa5982c9f69a0847dee4bcd0bab13b3a94
DenTaku <dentaku1412@gmail.com>
2020-09-27 00:44:19 +0900

    Learn recursive variant

--- 2/variant.ml
+++ 2/variant.ml
@@ -9,3 +9,12 @@ let price_of_dish d =
        |Soup m_and_g -> 90
        |Rice f -> (match f with Shake -> 90 | Katsuo -> 90 | Nori -> 80)
 ;;
+
+type menu = Smile | Add of {d: dish; next: menu};;
+
+let rec price_of_menu m =
+    match m with
+        Smile -> 0
+       |Add {d = d1; next = m'} -> price_of_dish d1 + price_of_menu m'
+;;
+
