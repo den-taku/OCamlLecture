2c3e8fed4b8435401f6760209822c889c6cfe227
DenTaku <dentaku1412@gmail.com>
2020-09-26 13:29:57 +0900

    Learn variable's range

---
+++ 1/range.ml
@@ -0,0 +1,11 @@
+let pi = 3.14;;
+let circle_area radius = pi *. radius *. radius;;
+circle_area 4.0;;
+
+let pi = 3.0;;
+circle_area 4.0;;
+
+circle_area pi;;
+
+let circle_area2 radius = pi *. radius *. radius;;
+circle_area2 4.0;;
